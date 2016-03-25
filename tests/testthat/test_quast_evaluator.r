0434528b481ea9400a0affa8ef8e22bdf6ff477d
pbelmann <pbelmann@cebitec.uni-bielefeld.de>
2016-03-25 10:49:38 +0100

    fix test run

--- tests/testthat/test_quast_evaluator.r
+++ tests/testthat/test_quast_evaluator.r
@@ -6,15 +6,27 @@ test_that("Script loads without an Error.", {
   expect_true(TRUE)
 })
 
-test_that("Script produces expected output files.", {
+testStatus <- test_that("Script produces expected output files.", {
   setwd("../..")
   outputDir = "tests/testthat/data/output"
-  dir.create(outputDir)
+
+  if(!dir.exists(outputDir)){
+  	dir.create(outputDir)
+  }
+
   cmd = paste("Rscript", "quast_evaluator_cli.r","tests/testthat/data/assemblers.tsv"," tests/testthat/data/info.tsv","-o", outputDir)
-  system(cmd)
+
+  if(system(cmd) != 0){
+      expect_true(FALSE)
+  } 
   
   outputFiles = c("references.html", "abundance.pdf", "abundance_no_points.pdf", "abundance-facet.pdf", "gc.pdf", "gc_no_points.pdf", "gc-facet.pdf")  
 
   lapply(outputFiles, function(outputFile) expect_true(file.exists(normalizePath(file.path(outputDir, outputFile)))))
    
 })
+
+
+if (!testStatus$passed){
+	quit(status=1)
+}
